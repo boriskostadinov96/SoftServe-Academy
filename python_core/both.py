f276027bb1382e75e050d4e5ec71d2989c468c6f
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-10-28 21:17:40 +0200

    Create both.py

---
+++ python_core/both.py
@@ -0,0 +1,11 @@
+def both(number1, number2):
+    if (number1 < 0 and number2 < 0) or (number1 > 0 and number2 > 0) or (number1 == 0 and number2 == 0):
+        return True
+    else:
+        return False
+
+
+print(both(6, 2))
+print(both(0, 0))
+print(both(-1, 2))
+print(both(0, 2))
