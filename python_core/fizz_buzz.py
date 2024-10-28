b5afaaad5a691e0930e3481c6fd71be6a82218ce
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-10-28 21:19:03 +0200

    Create fizz_buzz.py

---
+++ python_core/fizz_buzz.py
@@ -0,0 +1,18 @@
+def fizz_buzz(number):
+    if number % 3 == 0 and number % 5 == 0:
+        return "FizzBuzz"
+
+    elif number % 3 == 0:
+        return "Fizz"
+
+    elif number % 5 == 0:
+        return "Buzz"
+
+    else:
+        return str(number)
+
+
+print(fizz_buzz(3))
+print(fizz_buzz(5))
+print(fizz_buzz(15))
+print(fizz_buzz(4))
