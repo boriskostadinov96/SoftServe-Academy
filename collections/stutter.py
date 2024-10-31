3c1c0940eacd3f31c2236d09090a206863d803ab
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-10-31 16:29:13 +0200

    Create stutter.py

---
+++ collections/stutter.py
@@ -0,0 +1,7 @@
+def stutter(word):
+    first_two = word[:2]
+    return f"{first_two}... {first_two}... {word}?"
+
+print(stutter("incredible"))
+print(stutter("enthusiastic"))
+print(stutter("outstanding"))
