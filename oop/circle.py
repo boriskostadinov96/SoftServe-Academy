d9bba54fb2491283be1e3c43970ffcb2f30c8ac5
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-11-06 20:55:23 +0200

    Update circle.py

--- oop/circle.py
+++ oop/circle.py
@@ -1,15 +1,15 @@
 from math import pi
 
-
 class Circle:
     def __init__(self, radius=0):
         self.radius = radius
 
     def getArea(self):
-        return pi * (self.radius ** 2)
+        return round(pi * (self.radius ** 2))
 
     def getPerimeter(self):
-        return 2 * pi * self.radius
+        return round(2 * pi * self.radius)
+
 
 
 circy = Circle(11)
