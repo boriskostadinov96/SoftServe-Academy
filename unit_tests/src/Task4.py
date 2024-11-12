0b1c503d8789830fde57d4f284e7858cb63ad9b2
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-11-12 20:26:17 +0200

    Create Task4.py

---
+++ unit_tests/src/Task4.py
@@ -0,0 +1,7 @@
+# A Circle and Two Squares
+# Imagine a circle and two squares: a smaller and a bigger one. For the smaller one, the circle is a circumcircle and for the bigger one, an incircle.
+
+# Create a function, that takes an integer (radius of the circle) and returns the square area of the square inside the circle.
+
+def square_area(s):
+    return 2 * s ** 2
