4d630beed08837a0d2b667230f4c100ed5c09aa8
boriskostadinov96 <boriskostadinov96@gmail.com>
2024-11-14 20:23:40 +0200

    Update test_task20.py

--- unit_tests/tests/test_task20.py
+++ unit_tests/tests/test_task20.py
@@ -3,21 +3,26 @@ import unittest
 from src.Task20 import dice_game
 
 
-def test_dice_game():
-    assert dice_game([(1, 2), (3, 4), (5, 6)]) == 21
+class TestDiceGame(unittest.TestCase):
 
-    assert dice_game([(1, 1), (3, 4), (5, 6)]) == 0
+    def test_no_doubles(self):
+        self.assertEqual(dice_game([(1, 2), (3, 4), (5, 6)]), 1 + 2 + 3 + 4 + 5 + 6)
 
-    assert dice_game([(3, 2), (2, 2), (5, 6)]) == 0
+    def test_doubles_in_first_round(self):
+        self.assertEqual(dice_game([(1, 1), (3, 4), (5, 6)]), 0)
 
-    assert dice_game([(6, 3), (4, 2), (1, 1)]) == 0
+    def test_doubles_in_middle_round(self):
+        self.assertEqual(dice_game([(3, 2), (2, 2), (5, 6)]), 0)
 
-    assert dice_game([(2, 2), (4, 4), (6, 6)]) == 0
+    def test_valid_game_no_doubles(self):
+        self.assertEqual(dice_game([(6, 3), (4, 2), (1, 1)]), 0)
 
-    assert dice_game([(2, 3)]) == 5
+    def test_single_round_no_doubles(self):
+        self.assertEqual(dice_game([(1, 4)]), 1 + 4)
 
-    print("All tests passed!")
+    def test_single_round_with_doubles(self):
+        self.assertEqual(dice_game([(3, 3)]), 0)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
